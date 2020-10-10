532daf5e4409f70543fd7ed24fb54a25c2c228c9
doraemoe <jinyifan@outlook.com>
2020-10-10 12:20:34 +1000

    Initial Commit

---
+++ TestRepo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestRepo
+//
+//  Created by Jin Yifan on 10/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestRepo/TestRepoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestRepoApp.swift
+//  TestRepo
+//
+//  Created by Jin Yifan on 10/10/20.
+//
+
+import SwiftUI
+
+@main
+struct TestRepoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
