e9b66e4f41691908d261454738e4c4a7b7f5fc51
doraemoe <jinyifan@outlook.com>
2020-10-10 12:31:59 +1000

    test

--- TestRepoTests/TestRepoTests.swift
+++ TestRepoTests/TestRepoTests.swift
@@ -19,15 +19,11 @@ class TestRepoTests: XCTestCase {
     }
 
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertEqual(true, true)
     }
 
     func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        XCTAssertEqual(true, false)
     }
 
 }
